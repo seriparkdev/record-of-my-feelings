82ffcaec8bd4ece56d39290eb8087805a9cae7f0
seripark13 <qkrtpfl9228@gmail.com>
2022-06-30 19:15:31 +0900

    index 클릭 -> diary 출력

--- src/components/OpenDiary.tsx
+++
@@ -1,8 +0,0 @@
-export default function OpenDiary () {
-    return(
-        <div className="bg-black h-30">
-            <span>dfd</span>
-            
-        </div>
-    );
-}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,46 +34,48 @@ export default function Home() {
               emojiData.map((data) => {
                 if (emojiIndex <= 29 && item.emoji === data.name) {
                   item.number = ++emojiIndex;
-
                   return (
                     <span>
                       <button
+                        key={item.number}
                         className="cursor-pointer border-4 w-10 h-10"
                         value={emojiIndex}
                         onClick={(e: any) => {
                           setIndex(e.target.value);
-                          console.log(index);
                         }}
                       >
                         {emojiIndex}
+                        <img
+                          src={data.image}
+                          alt={data.name}
+                          className="w-10"
+                        />
                       </button>
-
-                      {/* <img src={data.image} alt={data.name} className="w-10" /> */}
                     </span>
                   );
                 } else if (emojiIndex === 30) {
-                  alert("30일까지만 입력 가능합니다")
                   ++emojiIndex;
+                  alert("30일까지만 입력 가능합니다");
                 }
-          
               })
             )}
         </span>
         <span className="border-4 border-black h-96 w-1/2">
-          {/* emoji위에 1~30 인덱스를 넣고, 인덱스를 클릭하면 onclick이벤트로
-          useState를 이용해 index 저장한다.
-          아이템에도 숫자를 넣고, 그 숫자와 index가 일치하면 그에 맞는 일기와
-          이모지를 오른쪽 페이지에 표시.  */}
-
           {items &&
             items.map((item) =>
               emojiData.map((data) => {
-                if (item.number === index) {
+                if (index == item.number) {
                   return (
-                    <>
-                      <span key={item.number}>{data.image}</span>
+                    <div>
+                      <img
+                        src={data.image}
+                        alt={data.name}
+                        key={item.number}
+                        className="w-10"
+                      />
                       <span>{item.number}</span>
-                    </>
+                      <span>{item.diary}</span>
+                    </div>
                   );
                 }
               })
