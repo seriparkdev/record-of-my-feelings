29be2cbc8df3bf33de62f225f64ffdc4c4fa3608
seripark13 <qkrtpfl9228@gmail.com>
2022-07-09 15:19:23 +0900

    emoji 출력 방법 변경

--- src/components/AddModal.tsx
+++ src/components/AddModal.tsx
@@ -1,3 +1,4 @@
+
 import React, { Dispatch, SetStateAction, useState } from "react"; // eslint-disable-line no-unused-vars
 
 export default function AddModal({
@@ -7,22 +8,24 @@ export default function AddModal({
 }: {
   isOpenModal: boolean;
   setIsOpenModal: Dispatch<SetStateAction<boolean>>;
-  addItemHandler: (item: { diary: string; emoji: string; number: number; image: string }) => void;
+  addItemHandler: (item: { diary: string; emoji: string; emojiImg: string; number: number; image: string }) => void;
 }) {
   const [diary, setDiary] = useState<string>("");
-  const [selected, setSelected] = useState("delight");
+  const [selected, setSelected] = useState("happy");
   const [urlImg, setUrlImg] = useState<string>('');
-
+  const [emojiFile, setEmojiFile] = useState<string>("/happy.png");
   const handlechange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelected(e.target.value);
+    setEmojiFile(`/${e.target.value}.png`)
   };
 
   function submitHandler(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
   const index = 0;
-    const item: { diary: string; emoji: string; number: number; image: string} = {
+    const item: { diary: string; emoji: string; emojiImg: string; number: number; image: string} = {
       diary: diary,
       emoji: selected,
+      emojiImg: emojiFile,
       number: index,
       image: urlImg
     };
@@ -30,6 +33,7 @@ export default function AddModal({
     setIsOpenModal(!isOpenModal);
   }
 
+ 
   return (
     <>
       <form onSubmit={submitHandler}>
@@ -37,24 +41,33 @@ export default function AddModal({
           className="fixed bg-black opacity-60 top-0 left-0 w-full h-full"
           onClick={() => setIsOpenModal(!isOpenModal)}
         ></div>
-        <div className="flex fixed top-2/4 left-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col p-8 rounded-lg bg-slate-300 w-[450px] h-[500px]">
-          <label htmlFor="diary">오늘 나의 하루</label>
-          <input
-            type="text"
+        <div className="flex fixed top-2/4 left-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col p-8 rounded-lg bg-[#f5f6f8]  w-10/12 h-[550px] text-center">
+        <span onClick={() => setIsOpenModal(!isOpenModal)} className="absolute top-0 right-0 pt-5 pr-9 cursor-pointer text-2xl hover:text-[#698476] ">
+            X
+          </span>
+
+          <div className="flex flex-row h-full">
+            <div className="flex flex-col mr-5 grow w-3/4">
+          <label htmlFor="diary" className="pb-4">오늘 나의 하루</label>
+          <textarea
             placeholder="오늘 있었던 일"
-            className="h-40"
+            className="p-7 h-full"
             id="diary"
             onChange={(e) => setDiary(e.target.value)
             }
             required
-          ></input>
-          
-          어떤 사진을 기록하고 싶나요?
-          <input type="text" placeholder="URL" onChange={(e) => setUrlImg(e.target.value)}></input>
+          ></textarea> 
+          </div>
 
-          <span>오늘 나의 기분은</span>
+          <div className="mt-20 w-1/4">
+          <div className="flex flex-col mb-10 m-auto">
+          <span className="pb-3">오늘의 사진</span>
+          <input type="text" placeholder="URL" onChange={(e) => setUrlImg(e.target.value)} className=""></input>
+          </div>
 
-          <div className="flex flex-row">
+          <div className="flex flex-col">
+          <span className="pb-3">오늘의 기분</span>
+        <div className="flex flex-row justify-center flex-wrap xl:flex-nowrap emojiBtn w-full">
               <input
                 type="radio"
                 name="emoji"
@@ -64,8 +77,9 @@ export default function AddModal({
                 onChange={handlechange}
                 
               />
-              <label htmlFor="happy"><img src= "/unclickedHappy.png" alt="happy" className="w-10 h-10" />
+              <label htmlFor="happy"><img src= "/Happy.png" alt="happy"  className="w-12 sm:w-14 cursor-pointer"/>
             </label>
+            
 
               <input
                 type="radio"
@@ -76,7 +90,7 @@ export default function AddModal({
                 onChange={handlechange}
               />
               <label htmlFor="sad">
-              <img src="/unclickedSad.png" alt="sad" className="h-10 w-10" />
+              <img src="/Sad.png" alt="sad" className="w-12 sm:w-14 cursor-pointer"/>
             </label>
 
            
@@ -89,7 +103,7 @@ export default function AddModal({
                 onChange={handlechange}
               />
                <label htmlFor="angry">
-              <img src="/unclickedAngry.png" alt="angry" className="h-10 w-10" />
+              <img src="/Angry.png" alt="angry" className="w-12 sm:w-14 cursor-pointer"/>
             </label>
 
           
@@ -102,7 +116,7 @@ export default function AddModal({
                 onChange={handlechange}
               />
                 <label htmlFor="tired">
-              <img src="/unclickedTired.png" alt="tired" className="h-10 w-10" />
+              <img src="/Tired.png" alt="tired" className="w-12 sm:w-14 cursor-pointer"/>
             </label>
 
           
@@ -115,15 +129,15 @@ export default function AddModal({
                 onChange={handlechange}
               />
                 <label htmlFor="monotonous">
-              <img src="/unclickedMonotonous.png" alt="monotonous" className="h-10 w-10" />
+              <img src="/Monotonous.png" alt="monotonous" className="w-12 sm:w-14 cursor-pointer"/>
             </label>
+           </div>
           </div>
-
-          <button type="button" onClick={() => setIsOpenModal(!isOpenModal)}>
-            닫기
-          </button>
-
-          <button type="submit">등록</button>
+            </div>
+            </div>
+            <span>
+          <button type="submit" className="bg-white w-10 h-8  rounded-md absolute bottom-0 right-0 hover:text-[#698476] mr-5 mb-5">등록</button>
+          </span>
         </div>
       </form>
     </>
